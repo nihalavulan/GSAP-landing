485290086db39a1d88570ba70517e17ad59d907e
nihal@0101 <nihal@mapout.com>
2025-09-07 17:11:39 +0530

    navbar done:

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-500">Hello, GSAP</h1>
-    </div>
+    <main className="" >
+        <Navbar />
+    </main>
   );
 };
 
 export default App;
+ 
